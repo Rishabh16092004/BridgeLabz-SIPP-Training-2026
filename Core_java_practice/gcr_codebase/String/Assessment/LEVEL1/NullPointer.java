a9c22c914efc52987d12e6ee8362594ec3c0e7b0
Rishabh16092004 <rishabhjaiswal16092004@gmail.com>
2026-06-13 23:24:28 +0530

    Rishabh : Initial commit

---
+++ Core_java_practice/gcr_codebase/String/Assessment/LEVEL1/NullPointer.java
@@ -0,0 +1,23 @@
+public class NullPointer {
+
+    static void generateException() {
+        String text = null;
+        System.out.println(text.length());
+    }
+
+    static void handleException() {
+        try {
+            String text = null;
+            System.out.println(text.length());
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException Handled");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // generateException();
+
+        handleException();
+    }
+}
