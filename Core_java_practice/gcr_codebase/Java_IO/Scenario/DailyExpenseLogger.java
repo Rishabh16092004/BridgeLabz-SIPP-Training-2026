9e0a805e0f26b903fc72920d8d6029ae468598b3
Rishabh16092004 <rishabhjaiswal16092004@gmail.com>
2026-06-19 23:11:37 +0530

    Rishabh : add Java_IO Scenario solution

---
+++ Core_java_practice/gcr_codebase/Java_IO/Scenario/DailyExpenseLogger.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class DailyExpenseLogger {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Expense Category: ");
+        String category = sc.nextLine();
+
+        System.out.print("Enter Amount: ");
+        int amount = sc.nextInt();
+
+        try {
+            FileWriter fw = new FileWriter("expenses.txt", true);
+            fw.write(category + " - " + amount);
+            fw.write(System.lineSeparator());
+            fw.close();
+            System.out.println("Expense saved successfully.");
+
+        } catch (IOException e) {
+            System.out.println("Error saving expense.");
+        }
+
+        sc.close();
+    }
+}
