f3ef1002fe8fa2165119264820840268f69faf85
Rishabh16092004 <rishabhjaiswal16092004@gmail.com>
2026-07-21 22:50:31 +0530

    Rishabh : add BST Assignment

---
+++ gcr_codebase/BST/DeleteBST.java
@@ -0,0 +1,67 @@
+class Node {
+    int val;
+    Node left, right;
+
+    Node(int val) {
+        this.val = val;
+    }
+}
+
+public class DeleteBST {
+    public static Node delete(Node root, int key) {
+        if (root == null)
+            return null;
+
+        if (key < root.val)
+            root.left = delete(root.left, key);
+
+        else if (key > root.val)
+            root.right = delete(root.right, key);
+
+        else {
+            if (root.left == null && root.right == null)
+                return null;
+
+            if (root.left == null)
+                return root.right;
+
+            if (root.right == null)
+                return root.left;
+
+            Node successor = root.right;
+
+            while (successor.left != null)
+                successor = successor.left;
+
+            root.val = successor.val;
+
+            root.right = delete(root.right, successor.val);
+        }
+        return root;
+    }
+
+    public static void inorder(Node root) {
+
+        if (root == null)
+            return;
+
+        inorder(root.left);
+        System.out.print(root.val + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(50);
+        root.left = new Node(30);
+        root.right = new Node(70);
+        root.left.left = new Node(20);
+        root.left.right = new Node(40);
+        root.right.left = new Node(60);
+        root.right.right = new Node(80);
+
+        root = delete(root, 70);
+
+        System.out.println("Inorder after deletion:");
+        inorder(root);
+    }
+}
---
+++ gcr_codebase/BST/InsertBST.java
@@ -0,0 +1,44 @@
+class Node {
+    int val;
+    Node left, right;
+
+    Node(int val) {
+        this.val = val;
+    }
+}
+
+public class InsertBST {
+    public static Node insert(Node root, int val) {
+        if (root == null)
+            return new Node(val);
+        if (val < root.val)
+            root.left = insert(root.left, val);
+        else if (val > root.val)
+            root.right = insert(root.right, val);
+
+        return root;
+    }
+
+    public static void inorder(Node root) {
+        if (root == null)
+            return;
+
+        inorder(root.left);
+        System.out.print(root.val + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+
+        Node root = new Node(50);
+        root.left = new Node(30);
+        root.right = new Node(70);
+        root.left.left = new Node(20);
+        root.left.right = new Node(40);
+
+        root = insert(root, 60);
+
+        System.out.println("Inorder after insertion:");
+        inorder(root);
+    }
+}
